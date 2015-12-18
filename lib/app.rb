5d555f458d403b02e37c70afcf8d2f9e7da775ba
Colt Steele <coltsteele1@gmail.com>
2015-12-17 23:23:11 -0800

    fix spacing issue

--- lib/app.rb
+++ lib/app.rb
@@ -16,9 +16,9 @@ puts "|_|                                       "
 
 
 # For each product in the data set:
-    # Print the name of the toy
-    # Print the retail price of the toy
-    # Calculate and print the total number of purchases
+  # Print the name of the toy
+  # Print the retail price of the toy
+  # Calculate and print the total number of purchases
   # Calcalate and print the total amount of sales
   # Calculate and print the average price the toy sold for
   # Calculate and print the average discount based off the average sales price
@@ -33,7 +33,7 @@ puts "|_|                                       "
 	puts
 
 # For each brand in the data set:
-    # Print the name of the brand
-    # Count and print the number of the brand's toys we stock
-    # Calculate and print the average price of the brand's toys
-    # Calculate and print the total sales volume of all the brand's toys combined
+  # Print the name of the brand
+  # Count and print the number of the brand's toys we stock
+  # Calculate and print the average price of the brand's toys
+  # Calculate and print the total revenue of all the brand's toy sales combined
